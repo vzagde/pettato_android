7533478ca485c738da989127db87ab7f8880080f
Vishal Z <itrs-671@ITRS-671s-MacBook-Pro.local>
2019-01-09 00:16:35 +0530

    Commit the new change for phot album

--- platforms/browser/www/js/util.js
+++ platforms/browser/www/js/util.js
@@ -348,7 +348,7 @@ function image_gallery() {
     navigator.camera.getPicture(shopper_register_onSuccess, shopper_register_onFail, {
         quality: 72,
         destinationType: Camera.DestinationType.FILE_URI,
-        sourceType: Camera.PictureSourceType.SAVEDPHOTOALBUM,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
         targetWidth: img_width,
         targetHeight: img_height,
         correctOrientation: true,
